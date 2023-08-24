0654c4a686300b5b6e2c09be0a0035240d2cc2a3
olabodeogun1 <olabodeogun1@gmail.com>
2023-08-24 05:50:28 -0700

    binary_trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,19 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to parent node
- * @value: input value
- * Return: pointer to new node
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: A pointer to the parent of the node to create.
+ * @value: The value to put in the new node.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *bt = NULL;
+binary_tree_t *node;
+node = malloc(sizeof(binary_tree_t));
+if (node == NULL)
+return (NULL);
 
-	bt = calloc(1, sizeof(binary_tree_t));
-	if (!bt)
-		return (NULL);
-	bt->parent = parent;
-	bt->n = value;
-	return (bt);
+node->n = value;
+node->parent = parent;
+node->left = NULL;
+node->right = NULL;
+
+return (node);
 }
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,26 +2,27 @@
 
 /**
  * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: pointer to parent node
- * @value: input value
- * Return: pointer to new node
+ * @parent: pointer to the node to insert the left-child.
+ * @value: value to store in the new node.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the created node.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *bt = NULL;
+binary_tree_t *newNode;
+if (parent == NULL)
+return (NULL);
+newNode = malloc(sizeof(binary_tree_t));
+if (newNode == NULL)
+return (NULL);
+newNode->n = value;
+newNode->parent = parent;
+newNode->left = parent->left;
+newNode->right = NULL;
+parent->left = newNode;
+if (newNode->left)
+newNode->left->parent = newNode;
 
-	if (!parent)
-		return (NULL);
-	bt = calloc(1, sizeof(binary_tree_t));
-	if (!bt)
-		return (NULL);
-	bt->n = value;
-	bt->parent = parent;
-	if (parent->left)
-	{
-		bt->left = parent->left;
-		bt->left->parent = bt;
-	}
-	parent->left = bt;
-	return (bt);
+return (newNode);
 }
